47d81463f5ef23ce5c9de91e285856cfca667c14
Mohammed Safwan <smohammed@fsid-iisc.in>
2025-03-26 13:20:29 +0530

    Added OAI threadCreate API

--- net_poll.c
+++ net_poll.c
@@ -1,6 +1,7 @@
 #define _GNU_SOURCE
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
 #include <pthread.h>
 #include <sched.h>
 #include <sys/resource.h>
@@ -17,6 +18,71 @@
 
 #define INTERFACE "eno1"
 #define NUM_THREADS 10  // Define the number of threads
+#define SCHED_OAI SCHED_RR
+#define OAI_PRIORITY_RT_LOW sched_get_priority_min(SCHED_OAI)
+#define OAI_PRIORITY_RT ((sched_get_priority_min(SCHED_OAI)+sched_get_priority_max(SCHED_OAI))/2)
+#define OAI_PRIORITY_RT_MAX sched_get_priority_max(SCHED_OAI)-2
+
+void threadCreate(pthread_t* t, void * (*func)(void*), void * param, char* name, int affinity, int priority)
+{
+  int ret;
+  bool set_prio = (geteuid() == 0); // Check if running as root for priority settings
+
+  pthread_attr_t attr;
+  ret = pthread_attr_init(&attr);
+  if (ret != 0) {
+    perror("pthread_attr_init");
+    exit(EXIT_FAILURE);
+  }
+
+  if (set_prio) {
+    ret = pthread_attr_setinheritsched(&attr, PTHREAD_EXPLICIT_SCHED);
+    if (ret != 0) {
+      perror("pthread_attr_setinheritsched");
+      exit(EXIT_FAILURE);
+    }
+
+    ret = pthread_attr_setschedpolicy(&attr, SCHED_FIFO); // Replacing SCHED_OAI with SCHED_FIFO
+    if (ret != 0) {
+      perror("pthread_attr_setschedpolicy");
+      exit(EXIT_FAILURE);
+    }
+
+    struct sched_param sparam = {0};
+    sparam.sched_priority = priority;
+    ret = pthread_attr_setschedparam(&attr, &sparam);
+    if (ret != 0) {
+      perror("pthread_attr_setschedparam");
+      exit(EXIT_FAILURE);
+    }
+    
+    printf("Creating thread %s with affinity %x, priority %d\n", name, affinity, priority);
+  } else {
+    affinity = -1;
+    priority = -1;
+    printf("Creating thread %s (no affinity, default priority)\n", name);
+  }
+
+  ret = pthread_create(t, &attr, func, param);
+  if (ret != 0) {
+    perror("pthread_create");
+    exit(EXIT_FAILURE);
+  }
+
+  pthread_setname_np(*t, name);
+  if (affinity != -1) {
+    cpu_set_t cpuset;
+    CPU_ZERO(&cpuset);
+    CPU_SET(affinity, &cpuset);
+    ret = pthread_setaffinity_np(*t, sizeof(cpu_set_t), &cpuset);
+    if (ret != 0) {
+      perror("pthread_setaffinity_np");
+      exit(EXIT_FAILURE);
+    }
+  }
+
+  pthread_attr_destroy(&attr);
+}
 
 void* poll_network(void* arg) {
     int sockfd;
@@ -24,22 +90,6 @@ void* poll_network(void* arg) {
     struct sockaddr saddr;
     socklen_t saddr_len = sizeof(saddr);
 
-    // Set thread name
-    pthread_setname_np(pthread_self(), "NetPollThread");
-
-    // Set CPU affinity to core 2
-    cpu_set_t cpuset;
-    CPU_ZERO(&cpuset);
-    CPU_SET(2, &cpuset);
-    pthread_setaffinity_np(pthread_self(), sizeof(cpu_set_t), &cpuset);
-
-    // Set highest real-time priority
-    struct sched_param param;
-    param.sched_priority = 99;
-    pthread_setschedparam(pthread_self(), SCHED_FIFO, &param);
-
-    // Set lowest nice value (-20)
-    setpriority(PRIO_PROCESS, 0, -20);
 
     // Create raw socket
     sockfd = socket(AF_PACKET, SOCK_RAW, htons(ETH_P_ALL));
@@ -81,29 +131,6 @@ void* poll_network(void* arg) {
 }
 
 void* poll_network_dummy(void* arg) {
-    int *cpu = (int *)arg;
-
-    char thread_name[30];
-    snprintf(thread_name, sizeof(thread_name), "NetPollThread%d", *cpu);
-    // Set thread name
-    pthread_setname_np(pthread_self(), thread_name);
-
-    // Set CPU affinity to core 2
-    cpu_set_t cpuset;
-    CPU_ZERO(&cpuset);
-    CPU_SET(*cpu, &cpuset);
-    pthread_setaffinity_np(pthread_self(), sizeof(cpu_set_t), &cpuset);
-
-    // Set highest real-time priority
-    struct sched_param param;
-    param.sched_priority = 99;
-    pthread_setschedparam(pthread_self(), SCHED_FIFO, &param);
-
-    // Set lowest nice value (-20)
-    setpriority(PRIO_PROCESS, 0, -20);
-
-    printf("Polling on CPU: %d\n", *cpu);
-    free(cpu);
 
     printf("Polling network interface: %s (100%% CPU Usage)\n", INTERFACE);
 
@@ -124,22 +151,19 @@ int main() {
 
     // Initialize thread attributes
     pthread_attr_init(&attr);
-    pthread_attr_setschedpolicy(&attr, SCHED_FIFO);
+    pthread_attr_setschedpolicy(&attr, SCHED_RR);
 
     // Create the polling thread
-    if (pthread_create(&thread, &attr, poll_network, NULL) != 0) {
-        perror("Thread creation failed");
-        return EXIT_FAILURE;
-    }
+    threadCreate(&thread, poll_network_dummy, NULL, "net-poll-2", 2, OAI_PRIORITY_RT_LOW);
+
+    int cpu;
+    char threadname[30];
 
     for (int i = 0; i < NUM_THREADS; i++) {
-        int *cpu = malloc(sizeof(int));  // Allocate memory for each thread
-        *cpu = 3 + (i);
-        if (pthread_create(&threads[i], &attr, poll_network_dummy, (void *)cpu) != 0) {
-            perror("Thread creation failed");
-            free(cpu);
-            return EXIT_FAILURE;
-        }
+        cpu = 3 + (i);
+        snprintf(threadname, sizeof(threadname), "net-poll-%d", cpu);
+        threadCreate(&threads[i], poll_network_dummy, NULL, threadname, cpu, OAI_PRIORITY_RT_LOW);
+        
     }
 
     // Wait for the thread
